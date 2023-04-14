049fb520b2cd41a68f019f4d50a0da2977df0d31
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-14 16:42:34 +0200

    FEAT controller + model + migration + seeder add

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreComicRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreComicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comic  $comic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comic $comic)
+    {
+        //
+    }
+
+}
