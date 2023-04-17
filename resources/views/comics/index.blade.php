b0eaa2083b21496b9859838d78161bb903e3de9f
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-17 16:04:27 +0200

    EDIT index view modified from cards to table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,17 +3,49 @@
 @section('content')
 
 <div>
+
   <div class="container">
-      <div class="row">
-          @foreach ($comics as $comic)
-              <div class="col-4">
-                <a href="{{ route('comics.show',$comic->id) }}">
-                  <img src="{{ $comic->thumb }}" alt="">
-                  <h3>{{ $comic->title }}</h3>
-                </a>
-              </div>   
-          @endforeach
+  <div class="row">
+      <div class="col-auto ms-auto">
+        <a class="btn btn-primary" href="{{ route('comics.create') }}">
+          Aggiungi Fumetto
+        </a>
       </div>
+    </div>
+  </div>
+  <div class="container">
+      
+    <table class="table">
+
+      <thead>
+        <tr>
+          <th>thumb</th>
+          <th>titolo</th>
+          <th>descrizione</th>
+          <th>serie</th>
+          <th>prezzo</th>
+          <th>id</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach ($comics as $comic)
+
+          <tr>
+            <td>
+              <img src="{{ $comic->thumb }}" alt="" style="width: 50px;">
+            </td>
+            <td>{{ $comic->title }}</td>
+            <td>{{ $comic->description }}</td>
+            <td>{{ $comic->series }}</td>
+            <td>{{ $comic->price }}</td>
+            <td>{{ $comic->id }}</td>
+          </tr>
+
+        @endforeach
+      </tbody>
+
+    </table>
   </div>
 </div>
 
