9356ce7044e16e90039d1c4fc40c5641ba2cabf4
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-19 15:52:25 +0200

    FEAT errors under input form added

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,32 +12,50 @@
 
       <div class="mb-3">
         <label for="title" class="form-label">Titolo</label>
-        <input type="text" class="form-control" id="title" name="title" value="{{ old('title')}}">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title')}}">
+        @error('title')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb')}}">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb')}}">
+        @error('thumb')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="price" class="form-label">Prezzo</label>
-        <input type="number" step="0.01" class="form-control" id="price" name="price" value="{{ old('price')}}">
+        <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price')}}">
+        @error('price')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="series" class="form-label">Serie</label>
-        <input type="text" class="form-control" id="series" name="series" value="{{ old('series')}}">
+        <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series')}}">
+        @error('series')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="sale-date" class="form-label">Data ingresso a mercato</label>
-        <input type="date" class="form-control" id="sale-date" name="sale_date" value="{{ old('sale_date')}}">
+        <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale-date" name="sale_date" value="{{ old('sale_date')}}">
+        @error('sale_date')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="type" class="form-label">Tipo</label>
-        <input type="text" class="form-control" id="type" name="type" value="{{ old('type')}}">
+        <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{ old('type')}}">
+        @error('type')
+          <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="mb-3">
@@ -51,6 +69,7 @@
 
     </form>
 
+    {{-- elenco degli errori
     @if ($errors->any())
       <div class="alert alert-danger">
           <ul>
@@ -59,7 +78,7 @@
               @endforeach
           </ul>
       </div>
-    @endif
+    @endif --}}
   </div>
 </div>
 
